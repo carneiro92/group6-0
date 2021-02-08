97a7039809cd15d11e85e741af7d096d371d9b60
adadgio <romain.bruckert.esc@gmail.com>
2021-02-08 17:05:50 +0100

    Changed configuration with "dotenv" for additional security.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
-import { Args } from "./config/args"
+//import { Args } from "./config/args"
+require('dotenv').config()
 import { Config } from "./config/config.service"
 import { DB } from "./service/db.service"
 
-Args.required("env", ["local", "test", "beta", "prod"])
-const ENV = Args.get("env")
-//console.log(`Environment is ${ENV}`)
+console.log(process.env)
 
-Config.init(ENV)
-console.log(Config.all())
-DB.init({ 
-    host: Config.get("DB_HOST"),
-    user: Config.get("DB_USER"),
-    password: Config.get("DB_PASSWORD"),
-    dbname: Config.get("DB_NAME"),
-})
+//Args.required("env", ["local", "test", "beta", "prod"])
+//const ENV = Args.get("env")
+////console.log(`Environment is ${ENV}`)
 
-DB.query("SELECT * FROM users")
-    .then(results => {
-        console.log(results)
-    }).catch(e => {
-        console.log(e)
-    })
+//Config.init(ENV)
+//console.log(Config.all())
+//DB.init({ 
+//    host: Config.get("DB_HOST"),
+//    user: Config.get("DB_USER"),
+//    password: Config.get("DB_PASSWORD"),
+//    dbname: Config.get("DB_NAME"),
+//})
+
+//DB.query("SELECT * FROM users")
+//    .then(results => {
+//        console.log(results)
+//    }).catch(e => {
+//        console.log(e)
+//    })
