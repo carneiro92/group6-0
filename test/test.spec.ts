40b036dc4acc0ee959bd921f3a3abd695ba897ea
adadgio <romain.bruckert.esc@gmail.com>
2021-02-08 15:07:11 +0100

    no message

---
+++ test/test.spec.ts
@@ -0,0 +1,20 @@
+import * as assert from "assert"
+import * as chai from "chai"
+const expect = chai.expect
+import { User } from "../src/model/User"
+
+const data = {
+    id: 5,
+    surname: "Ben",
+    name: "John",
+}
+const user = new User(data)
+
+describe("Test models", () => {
+
+    it("Test if user model is valid", () => {
+        expect(user.id).to.equal(5)
+        expect(user.name).to.equal("John")
+        expect(user).to.not.have.property("surname")
+    })
+})
