fcb5481b49a553b04c88b3d22e84012fd89ab336
Carneiro92 <jorgecarneiro092@gmail.com>
2021-02-09 15:17:54 +0100

    fix bash

--- pre-test.sh
+++ pre-test.sh
@@ -1,2 +1,2 @@
-cat ./test/create_db.sql | mysql -h127.0.0.1 -uroot -proot
-cat ./test/mockup_db.sql | mysql -h127.0.0.1 -uroot -proot simplon_groupx_test
+cat ./test/create_db.sql | mysql -h127.0.0.1 -uPhilipNorris -ptest2
+cat ./test/mockup_db.sql | mysql -h127.0.0.1 -uPhilipNorris -ptest2 simplon_groupx_test
